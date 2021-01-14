dc5c02897d82f7423930bb08534ce0dc27ba757e
Fabio Petrone <fa610movic90@gmail.com>
2021-01-14 20:24:55 +0100

    che coglione

--- ss/js/snake.js
+++ ss/js/snake.js
@@ -52,7 +52,7 @@ var snake=g[g_width/2][3];
 console.log(snake);
 snakeDirection = 'R';
 
-var apple=[(g_width)*(Math.floor(Math.random()*(g_width+1)))];
+var apple=[Math.floor(Math.random()*(g_width+1))];
 console.log(apple);
 
 // posso provare generazione mela?è casuale
