fbb9d24564f0af809cd340260a33d659db3fa06a
fabio <fabio.petrone@outlook.it>
2021-05-17 10:57:36 +0200

    ecma option

--- my-filters/js/script.js
+++ my-filters/js/script.js
@@ -2,6 +2,12 @@ let grayscale = document.getElementById('grayscale');
 let blur = document.getElementById('blur');
 let brightness = document.getElementById('brightness');
 const preview = document.querySelector('img');
+let allFilters='';
+
+$( document ).ready(function() {
+  deletefilters();
+});
+
 function previewFile() {
     const file = document.querySelector('input[type=file]').files[0];
     const reader = new FileReader();
@@ -15,9 +21,14 @@ function previewFile() {
   }
 
   function filter(id,value,unity){
-     preview.style.filter=id+'('+value+unity+')';
+    //  preview.style.filter=id+'('+value+unity+')';
+     preview.style.filter=`${id}(${value}${unity})`;
+    //  ecma
   }
 
 function deletefilters(){
   preview.style.filter='none';
+  grayscale.value = 0;
+  blur.value = 0;
+  brightness.value = 100;
 }
